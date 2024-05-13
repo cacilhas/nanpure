a9ed15b8d3c2ca92647206f24f7b5f06ed5a6ac3
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-05-13 00:12:36 -0300

    add start buttons [WIP]

--- src/scene/start_menu.rs
+++ src/scene/start_menu.rs
@@ -2,7 +2,7 @@ use std::os::raw::c_int;
 
 use super::{action::Action, Scene};
 use crate::{colors, fonts::get_font};
-use raylib::{draw, enums::KeyboardKey, rl_str, Font, Vector2};
+use raylib::{draw, enums::KeyboardKey, rl_str, Font, Rectangle, Vector2};
 
 #[derive(Debug)]
 pub struct StartMenu {
@@ -71,6 +71,109 @@ impl StartMenu {
             1.0,
             colors::DARKCYAN,
         );
+        let mouse_pos = raylib::GetMousePosition();
+
+        let bottom = bottom + size.y + 64.0;
+        let size = raylib::MeasureTextEx(font, rl_str!("1. Extremely Easy"), 64.0, 1.0);
+        let extr_easy_bt = Rectangle {
+            x: (width - size.x) / 2.0,
+            y: bottom,
+            width: size.x,
+            height: size.y,
+        };
+        let position = Vector2 {
+            x: extr_easy_bt.x,
+            y: extr_easy_bt.y,
+        };
+        let tint = if raylib::CheckCollisionPointRec(mouse_pos, extr_easy_bt) {
+            colors::BLACK
+        } else {
+            colors::DARKGRAY
+        };
+        raylib::DrawTextEx(
+            font,
+            rl_str!("1. Extremely Easy"),
+            position,
+            64.0,
+            1.0,
+            tint,
+        );
+
+        let bottom = bottom + size.y + 12.0;
+        let size = raylib::MeasureTextEx(font, rl_str!("2. Easy"), 64.0, 1.0);
+        let easy_bt = Rectangle {
+            x: (width - size.x) / 2.0,
+            y: bottom,
+            width: size.x,
+            height: size.y,
+        };
+        let position = Vector2 {
+            x: easy_bt.x,
+            y: easy_bt.y,
+        };
+        let tint = if raylib::CheckCollisionPointRec(mouse_pos, easy_bt) {
+            colors::BLACK
+        } else {
+            colors::DARKGRAY
+        };
+        raylib::DrawTextEx(font, rl_str!("2. Easy"), position, 64.0, 1.0, tint);
+
+        let bottom = bottom + size.y + 12.0;
+        let size = raylib::MeasureTextEx(font, rl_str!("3. Medium"), 64.0, 1.0);
+        let medium_bt = Rectangle {
+            x: (width - size.x) / 2.0,
+            y: bottom,
+            width: size.x,
+            height: size.y,
+        };
+        let position = Vector2 {
+            x: medium_bt.x,
+            y: medium_bt.y,
+        };
+        let tint = if raylib::CheckCollisionPointRec(mouse_pos, medium_bt) {
+            colors::BLACK
+        } else {
+            colors::DARKGRAY
+        };
+        raylib::DrawTextEx(font, rl_str!("3. Medium"), position, 64.0, 1.0, tint);
+
+        let bottom = bottom + size.y + 12.0;
+        let size = raylib::MeasureTextEx(font, rl_str!("4. Hard"), 64.0, 1.0);
+        let hard_bt = Rectangle {
+            x: (width - size.x) / 2.0,
+            y: bottom,
+            width: size.x,
+            height: size.y,
+        };
+        let position = Vector2 {
+            x: hard_bt.x,
+            y: hard_bt.y,
+        };
+        let tint = if raylib::CheckCollisionPointRec(mouse_pos, hard_bt) {
+            colors::BLACK
+        } else {
+            colors::DARKGRAY
+        };
+        raylib::DrawTextEx(font, rl_str!("4. Hard"), position, 64.0, 1.0, tint);
+
+        let bottom = bottom + size.y + 12.0;
+        let size = raylib::MeasureTextEx(font, rl_str!("5. Fiendish"), 64.0, 1.0);
+        let fiendish_bt = Rectangle {
+            x: (width - size.x) / 2.0,
+            y: bottom,
+            width: size.x,
+            height: size.y,
+        };
+        let position = Vector2 {
+            x: fiendish_bt.x,
+            y: fiendish_bt.y,
+        };
+        let tint = if raylib::CheckCollisionPointRec(mouse_pos, fiendish_bt) {
+            colors::BLACK
+        } else {
+            colors::DARKGRAY
+        };
+        raylib::DrawTextEx(font, rl_str!("5. Fiendish"), position, 64.0, 1.0, tint);
 
         raylib::EndMode2D();
     }

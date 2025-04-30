f39f097c17074981ff543a4a36ba25d245c3ce14
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2025-04-30 16:37:53 -0300

    configure main window

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
 use bevy::prelude::*;
+use bevy::window::{
+    EnabledButtons,
+    PresentMode,
+    WindowTheme,
+};
 
 mod consts;
 mod exit;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: consts::TITLE.into(),
+                    resolution: consts::RESOLUTION.into(),
+                    present_mode: PresentMode::AutoVsync,
+                    prevent_default_event_handling: true,
+                    window_theme: Some(WindowTheme::Light),
+                    enabled_buttons: EnabledButtons {
+                        minimize: true,
+                        maximize: false,
+                        close: true,
+                    },
+                    visible: true,
+                    ..default()
+                }),
+                close_when_requested: true,
+                ..default()
+            })
+        )
         .add_systems(Update, exit::exit_system)
         .run();
 }
